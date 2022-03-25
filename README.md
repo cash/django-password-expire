0749ae89c22ecb51e401cf27fcc45aa74cd8dd76
Cash Costello <cash.costello@jhuapl.edu>
2022-03-24 20:42:24 -0400

    initial model

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# django-password-expire
-Django app to manage password expiration
+# Django password expiration app
+This app provides configurable expiration of passwords.
+
+## Installation
+ 1. `pip install django-password-expire`
+ 2. Add `password-expire` to `INSTALLED_APPS`
+ 3. 
+ 4. Run `python migrate` to create the required database tables
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="django-password-expire",
+    version="0.1",
+    description="Django app for forcing password expiration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cash/django-password-expire",
+    author="Cash Costello",
+    author_email="cash.costello@gmail.com",
+    license="BSD",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+    ]
+)
